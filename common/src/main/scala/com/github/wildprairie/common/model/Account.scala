a894df33d267185f65913d73bd68bb5bf89e0f56
Jacek Malec <jac3km4@gmail.com>
2017-05-19 23:26:52 +0200

    implement basic account authentication

---
+++ common/src/main/scala/com/github/wildprairie/common/model/Account.scala
@@ -0,0 +1,24 @@
+package com.github.wildprairie.common.model
+
+import com.github.wakfutcp.protocol.common.Community
+
+case class Account(
+  id: Int,
+  login: String,
+  hashedPassword: String,
+  community: Community
+)
+object Account {
+  import com.github.wildprairie.common.model.Database.context._
+
+  implicit val encodeCommunity: MappedEncoding[Community, Int] =
+    MappedEncoding(_.value)
+  implicit val decodeCommunity: MappedEncoding[Int, Community] =
+    MappedEncoding(Community.withValue)
+
+  def getAccountByLogin(login: String): Quoted[Query[Account]] =
+    quote {
+      query[Account]
+        .filter(_.login == lift(login))
+    }
+}
---
+++ common/src/main/scala/com/github/wildprairie/common/model/Database.scala
@@ -0,0 +1,10 @@
+package com.github.wildprairie.common.model
+
+import io.getquill.{PostgresAsyncContext, SnakeCase}
+
+/**
+  * Created by jacek on 19.05.17.
+  */
+object Database {
+  lazy val context = new PostgresAsyncContext[SnakeCase]("db")
+}
