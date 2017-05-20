75f6bb9e5b9da9868652f9df8b1e0b1a997662e7
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2017-05-20 03:05:40 +0200

    token cleanup

--- common/src/main/scala/com/github/wildprairie/common/actors/world/TokenAuthenticator.scala
+++ common/src/main/scala/com/github/wildprairie/common/actors/world/TokenAuthenticator.scala
@@ -1,5 +1,6 @@
 package com.github.wildprairie.common.actors.world
 
+import java.time.Instant
 import java.util.UUID
 
 import akka.actor.{Actor, ActorLogging, Props}
@@ -10,33 +11,55 @@ import com.github.wildprairie.common.actors.shared.Authenticator
   * Created by hussein on 19/05/17.
   */
 object TokenAuthenticator {
+  import scala.concurrent.duration._
+
   def props(): Props =
     Props(classOf[TokenAuthenticator])
 
   sealed trait Message
   final case class TokenGenerationRequest(account: AccountInformation) extends Message
   final case class TokenGenerationResult(token: String, account: AccountInformation) extends Message
+  final case object CleanupTokens extends Message
+
+  val TOKEN_CLEANUP_DELAY: FiniteDuration = 1.minute
+  val TOKEN_TIMEOUT: FiniteDuration = 1.minute
 }
 
 class TokenAuthenticator extends Actor
   with ActorLogging with Authenticator[String, AccountInformation] {
   import TokenAuthenticator._
   import Authenticator._
+  import context._
+
+  override def preStart(): Unit =
+    system.scheduler.schedule(
+      TOKEN_CLEANUP_DELAY,
+      TOKEN_CLEANUP_DELAY,
+      self,
+      CleanupTokens
+    )
 
   override def receive: Receive =
     handleRequest(Map())
 
-  def handleRequest(tokens: Map[String, AccountInformation]): Receive = {
+  def handleRequest(tokens: Map[String, (Long, AccountInformation)]): Receive = {
+    case CleanupTokens =>
+      log.info("cleaning up invalid tokens")
+      val validTokens = tokens.filter(_._2._1 > Instant.now.getEpochSecond)
+      context.become(handleRequest(validTokens))
+
     case TokenGenerationRequest(account) =>
       val token = UUID.randomUUID().toString()
+      val timeout = Instant.now.getEpochSecond + TOKEN_TIMEOUT.toSeconds
+      val mapValue = (timeout, account)
       log.info(s"generated auth token: account=$account, token=$token")
       sender ! TokenGenerationResult(token, account)
-      context.become(handleRequest(tokens + (token -> account)))
+      context.become(handleRequest(tokens + (token -> mapValue)))
 
     case Authenticate(token: String) =>
       log.info(s"authenticating user with token=$token")
       tokens.get(token) match {
-        case Some(account) =>
+        case Some((_, account)) =>
           sender ! Success(token, account)
           context.become(handleRequest(tokens - token))
 
